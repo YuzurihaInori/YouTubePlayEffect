682ff94ff776ac1a939130e2292677cd8ca8d53a
JOJO <839604535@qq.com>
2015-12-04 19:11:33 +0800

    微调

--- app/src/main/java/sausure/youtubeplayeffectsample/MainActivity.java
+++ app/src/main/java/sausure/youtubeplayeffectsample/MainActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements YouTubePlayEffect
     private SurfaceHolder holder;
     private YouTubePlayEffect mEffectPlayer;
     private Button testButton;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,13 @@ public class MainActivity extends AppCompatActivity implements YouTubePlayEffect
 
         testButton = (Button) findViewById(R.id.test);
         testButton.setOnClickListener(this);
+
+        listView = (ListView) findViewById(R.id.list_view);
+        initList();
+    }
+
+    private void initList(){
+        listView.setAdapter(ArrayAdapter.createFromResource(this,R.array.test_list,android.R.layout.simple_list_item_1));
     }
 
     private void playVideo(){
@@ -56,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements YouTubePlayEffect
     public void onDisappear(int direct) {
         mediaPlayer.pause();
         testButton.setVisibility(View.VISIBLE);
-        Toast.makeText(this,"destroy-direct-"+(direct == YouTubePlayEffect.SLIDE_TO_LEFT ?
+        Toast.makeText(this,"destroy-direction-"+(direct == YouTubePlayEffect.SLIDE_TO_LEFT ?
                 "left" : "right"),Toast.LENGTH_SHORT).show();
     }
 
@@ -76,8 +86,8 @@ public class MainActivity extends AppCompatActivity implements YouTubePlayEffect
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onDestroy() {
+        super.onDestroy();
         if(mediaPlayer.isPlaying()){
             mediaPlayer.stop();
         }
